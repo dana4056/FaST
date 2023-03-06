423cb7a61a043a8f1a2376288e55fa7c1632dfc2
이소민 <esom735@gmail.com>
2023-03-06 13:20:54 +0900

    [FE] feat: login page INPUT 스타일 적용

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,6 +1,14 @@
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function LoginPage() {
+  const navigate = useNavigate();
+
+  // 로그인 하러가기
+  const goLogin = () => {
+    navigate('/home');
+  };
+
   return (
     <div className="login-page">
       <div className="login-page__logo">
@@ -10,24 +18,32 @@ export default function LoginPage() {
         <span className="logo__t">T</span>
       </div>
 
-      <form className="sign-up-page">
-        <div className="sign-up-page__row">
-          <div className="sign-up-page__row__text">
-            <text className="sign-up-page__text">이메일</text>
+      <form className="login-page">
+        <div className="login-page__row">
+          <div className="login-page__row__text">
+            <text className="login-page__text">이메일</text>
           </div>
-          <input className="card sign-up-page__input" type="email" />
+          <input className="card login-page__input" type="email" />
         </div>
-        <div className="sign-up-page__row">
-          <div className="sign-up-page__row__text">
-            <text className="sign-up-page__text">비밀번호</text>
+        <div className="login-page__row">
+          <div className="login-page__row__text">
+            <text className="login-page__text">비밀번호</text>
           </div>
           <input
-            className="card sign-up-page__input"
+            className="card login-page__input"
             type="password"
             placeholder="영어, 숫자 8~15자리"
           />
         </div>
       </form>
+
+      <button
+        className="card login-page__button"
+        type="button"
+        onClick={goLogin}
+      >
+        로그인
+      </button>
     </div>
   );
 }
