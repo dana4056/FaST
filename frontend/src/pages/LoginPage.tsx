cdb4103822aa640c492ff935888f8c6dd4c5a442
이소민 <esom735@gmail.com>
2023-03-06 11:10:10 +0900

    [FE] feat: login page logo 배치

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -2,8 +2,13 @@ import React, { useEffect, useState } from 'react';
 
 export default function LoginPage() {
   return (
-    <div>
-      <p>Login Page !!!!!!</p>
+    <div className="login-page">
+      <div className="login-page__logo">
+        <span className="logo__f">F</span>
+        <span className="logo__a">a</span>
+        <span className="logo__s">S</span>
+        <span className="logo__t">T</span>
+      </div>
     </div>
   );
 }
