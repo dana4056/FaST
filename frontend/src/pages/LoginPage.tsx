d318c7b7e6510f5fa7763c7ee47dc3de539aa2d4
이소민 <esom735@gmail.com>
2023-03-06 12:48:02 +0900

    [FE] feat: login page 이메일, 비밀번호 form 추가

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -9,6 +9,25 @@ export default function LoginPage() {
         <span className="logo__s">S</span>
         <span className="logo__t">T</span>
       </div>
+
+      <form className="sign-up-page">
+        <div className="sign-up-page__row">
+          <div className="sign-up-page__row__text">
+            <text className="sign-up-page__text">이메일</text>
+          </div>
+          <input className="card sign-up-page__input" type="email" />
+        </div>
+        <div className="sign-up-page__row">
+          <div className="sign-up-page__row__text">
+            <text className="sign-up-page__text">비밀번호</text>
+          </div>
+          <input
+            className="card sign-up-page__input"
+            type="password"
+            placeholder="영어, 숫자 8~15자리"
+          />
+        </div>
+      </form>
     </div>
   );
 }
