028d8dda49c67881db70fee57cfefc14bf8b30da
고태진 <thereisnotruth12@gmail.com>
2023-03-09 13:01:03 +0900

    [FE] feat: 광안대교 모델 추가

--- frontend/src/pages/TestPage.tsx
+++ frontend/src/pages/TestPage.tsx
@@ -3,6 +3,7 @@ import Landmark from '../components/Landmark';
 import Sungnyemun from '../assets/blender/Sungnyemun';
 import SeoulTower from '../assets/blender/SeoulTower';
 import Cheomseongdae from '../assets/blender/Cheomseongdae';
+import Gwangandaegyo from '../assets/blender/Gwangandaegyo';
 
 function TestPage() {
   return (
@@ -10,6 +11,7 @@ function TestPage() {
       <Landmark Model={Sungnyemun} cameraPosition={[0, 2, 8]} />
       <Landmark Model={SeoulTower} cameraPosition={[0, 2, 9]} />
       <Landmark Model={Cheomseongdae} cameraPosition={[0, 2, 8]} />
+      <Landmark Model={Gwangandaegyo} cameraPosition={[0, 2, 5]} />
     </div>
   );
 }
