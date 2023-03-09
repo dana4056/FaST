4a05ca5a8f75179752baf8943f11278fb59b5aee
sehee <parksehee6938@gmail.com>
2023-03-09 14:36:49 +0900

    [FE] feat: map 설정

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -1,73 +1,229 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import koreamap from '../assets/images/koreamap.png';
-import photocardimg from '../assets/images/photocardimg.jpeg';
-import sample1 from '../assets/images/sample-images/sample_1.jpg';
-import sample2 from '../assets/images/sample-images/sample_2.jpg';
-import sample3 from '../assets/images/sample-images/sample_3.jpg';
+import Kakaomap from './Kakaomap';
 
 function Map() {
+  const cntData = {
+    seoul_cnt: 4,
+    gyeonggi_cnt: 1,
+    incheon_cnt: 2,
+    sejong_cnt: 5,
+    gangwon_cnt: 3,
+    daejeon_cnt: 6,
+    daegu_cnt: 2,
+    chungcheongbuk_cnt: 1,
+    chungcheongnam_cnt: 4,
+    jeollabuk_cnt: 2,
+    jeollanam_cnt: 1,
+    gwangju_cnt: 5,
+    gyeongsangbuk_cnt: 3,
+    gyeongsangnam_cnt: 6,
+    ulsan_cnt: 1,
+    busan_cnt: 2,
+    jeju_cnt: 0,
+    ulleungdo_cnt: 3,
+    dokdo_cnt: 5,
+  };
+
+  const seoulColor = `color${cntData.seoul_cnt}`;
+  const gyeonggiColor = `color${cntData.gyeonggi_cnt}`;
+  const incheonColor = `color${cntData.incheon_cnt}`;
+  const sejongColor = `color${cntData.sejong_cnt}`;
+  const gangwonColor = `color${cntData.gangwon_cnt}`;
+  const daejeonColor = `color${cntData.daejeon_cnt}`;
+  const daeguColor = `color${cntData.daegu_cnt}`;
+  const chungcheongbukColor = `color${cntData.chungcheongbuk_cnt}`;
+  const chungcheongnamColor = `color${cntData.chungcheongnam_cnt}`;
+  const jeollabukColor = `color${cntData.jeollabuk_cnt}`;
+  const jeollanamColor = `color${cntData.jeollanam_cnt}`;
+  const gwangjuColor = `color${cntData.gwangju_cnt}`;
+  const gyeongsangbukColor = `color${cntData.gyeongsangbuk_cnt}`;
+  const gyeongsangnamColor = `color${cntData.gyeongsangnam_cnt}`;
+  const ulsanColor = `color${cntData.ulsan_cnt}`;
+  const busanColor = `color${cntData.busan_cnt}`;
+  const jejuColor = `color${cntData.jeju_cnt}`;
+  const ulleungdoColor = `color${cntData.ulleungdo_cnt}`;
+  const dokdoColor = `color${cntData.dokdo_cnt}`;
+
+  const [clickGangwon, setClickGangwon] = useState(false);
+
   return (
     <div className="koreamap">
       <div className="total__map">
         <img src={koreamap} alt="koreamap" className="base__map" />
-        <div className="seoul">
-          <img src={sample1} alt="sample1" />
-        </div>
-        <div className="gyunggi">
-          <img src={sample2} alt="sample2" />
-        </div>
-        <div className="incheon">
-          <img src={sample3} alt="sample3" />
+        <div className="gyeonggi">
+          <div className="gyeonggi_back">
+            <div className={gyeonggiColor}>
+              <button type="button">경기도</button>
+            </div>
+          </div>
         </div>
         <div className="gangwon">
-          <img src={photocardimg} alt="photocardimg" />
+          <div className="gangwon_back">
+            <div className={gangwonColor}>
+              <button type="button" className="mapbtn">
+                강원도
+              </button>
+            </div>
+          </div>
         </div>
-        <div className="gyeongsangbuk">
-          <img src={sample3} alt="sample3" />
+        <div className="seoul">
+          <div className="seoul_back">
+            <div className={seoulColor}>
+              <button type="button" className="mapbtn">
+                강원도
+              </button>
+            </div>
+          </div>
         </div>
-        <div className="daejun">
-          <img src={sample1} alt="sample1" />
-        </div>
-        <div className="daegoo">
-          <img src={photocardimg} alt="photocardimg" />
-        </div>
-        <div className="chungcheongbuk">
-          <img src={sample1} alt="sample1" />
+        <div className="incheon">
+          <div className="incheon_back">
+            <div className={incheonColor}>
+              <button type="button" className="mapbtn">
+                강원도
+              </button>
+            </div>
+          </div>
         </div>
         <div className="chungcheongnam">
-          <img src={photocardimg} alt="photocardimg" />
+          <div className="chungcheongnam_back">
+            <div className={chungcheongnamColor}>
+              <button type="button" className="mapbtn">
+                강원도
+              </button>
+            </div>
+          </div>
         </div>
         <div className="jeollabuk">
-          <img src={sample1} alt="sample1" />
+          <div className="jeollabuk_back">
+            <div className={jeollabukColor}>
+              <button type="button" className="mapbtn">
+                강원도
+              </button>
+            </div>
+          </div>
         </div>
         <div className="jeollanam">
-          <img src={sample2} alt="sample2" />
+          <div className="jeollanam_back">
+            <div className={jeollanamColor}>
+              <button type="button" className="mapbtn">
+                강원도
+              </button>
+            </div>
+          </div>
         </div>
         <div className="gwangju">
-          <img src={photocardimg} alt="photocardimg" />
+          <div className="gwangju_back">
+            <div className={gwangjuColor}>
+              <button type="button" className="mapbtn">
+                강원도
+              </button>
+            </div>
+          </div>
         </div>
-        <div className="gyeongsangnam">
-          <img src={sample1} alt="sample1" />
-        </div>
-        <div className="ulsan">
-          <img src={photocardimg} alt="photocardimg" />
-        </div>
-        <div className="busan">
-          <img src={photocardimg} alt="photocardimg" />
+        <div className="chungcheongbuk">
+          <div className="chungcheongbuk_back">
+            <div className={chungcheongbukColor}>
+              <button type="button" className="mapbtn">
+                강원도
+              </button>
+            </div>
+          </div>
         </div>
         <div className="sejong">
-          <img src={photocardimg} alt="photocardimg" />
+          <div className="sejong_back">
+            <div className={sejongColor}>
+              <button type="button" className="mapbtn">
+                강원도
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="daejeon">
+          <div className="daejeon_back">
+            <div className={daejeonColor}>
+              <button type="button" className="mapbtn">
+                강원도
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="gyeongsangbuk">
+          <div className="gyeongsangbuk_back">
+            <div className={gyeongsangbukColor}>
+              <button type="button" className="mapbtn">
+                강원도
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="ulsan">
+          <div className="ulsan_back">
+            <div className={ulsanColor}>
+              <button type="button" className="mapbtn">
+                강원도
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="gyeongsangnam">
+          <div className="gyeongsangnam_back">
+            <div className={gyeongsangnamColor}>
+              <button type="button" className="mapbtn">
+                강원도
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="daegu">
+          <div className="daegu_back">
+            <div className={daeguColor}>
+              <button type="button" className="mapbtn">
+                강원도
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="busan">
+          <div className="busan_back">
+            <div className={busanColor}>
+              <button type="button" className="mapbtn">
+                강원도
+              </button>
+            </div>
+          </div>
         </div>
         <div className="jeju">
-          <img src={photocardimg} alt="photocardimg" />
+          <div className="jeju_back">
+            <div className={jejuColor}>
+              <button type="button" className="mapbtn">
+                강원도
+              </button>
+            </div>
+          </div>
         </div>
         <div className="ulleungdo">
-          <img src={photocardimg} alt="photocardimg" />
+          <div className="ulleungdo_back">
+            <div className={ulleungdoColor}>
+              <button type="button" className="mapbtn">
+                강원도
+              </button>
+            </div>
+          </div>
         </div>
         <div className="dokdo">
-          <img src={photocardimg} alt="photocardimg" />
+          <div className="dokdo_back">
+            <div className={dokdoColor}>
+              <button type="button" className="mapbtn">
+                강원도
+              </button>
+            </div>
+          </div>
         </div>
+      </div>
+      <div className="kakao__map">
+        <Kakaomap />
       </div>
     </div>
   );
