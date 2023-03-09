d52b51fb7d1acca7fd19e91932b9e08b7c8fe760
dhwnsghdhkd <dhwnsghdhkd@naver.com>
2023-03-09 14:35:52 +0900

    [BE] feat : 회원탈퇴, 닉네임 변경, 임시비밀번호 발급 API 완료

--- backend/src/main/java/a402/FaST/Controller/UserController.java
+++ backend/src/main/java/a402/FaST/Controller/UserController.java
@@ -13,12 +13,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
-
+@CrossOrigin
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
@@ -34,9 +36,13 @@ public class UserController {
     @PostMapping("/user")
     @Operation(summary = "회원가입 API =>  유저 정보를 입력받아 회원가입하는 API 입니다.",
             description = "json 형식 데이터 -> (String : email, String : password, String : nickname) " +
-            " => 회원가입에 완료한 User 정보를 Return 해줍니다.")
+            " => 회원가입에 완료한 User 정보를 Return 해줍니다.  => 만약 이메일 중복이나 닉네임 중복이면 error 메시지 반환")
     public ResponseEntity<UserResponseDto> signup(@Valid @RequestBody UserRequestDto requestDto) {
-        return ResponseEntity.ok(userService.signup(requestDto));
+        UserResponseDto responseDto = null;
+        if (userService.checkMail(requestDto) && userService.checkNickname(requestDto)){
+            responseDto = userService.signup(requestDto);
+        }
+        return ResponseEntity.ok(responseDto);
     }
 
     // 로그인 메소드
@@ -45,7 +51,6 @@ public class UserController {
             description = "json 형식 데이터 -> (String : email, String : password)" +
             " => 로그인에 완료한 User 정보와 Token 정보를 Return 해줍니다.")
     private ResponseEntity<Map<String, Object>> login(@RequestBody UserRequestDto requestDto) {
-
         Map<String, Object> resultMap = new HashMap<>();
         HttpStatus status = HttpStatus.OK;
         TokenDto TokenResponseDto = null;
@@ -66,69 +71,49 @@ public class UserController {
     @Operation(summary = "이메일 전송 API =>  이메일 입력을 통해 이메일을 전송하는 API 입니다.",
             description = "json 형식 데이터 -> (String : email)" +
                     " => 이메일로 전송한 인증번호를 Return 해줍니다.")
-    public String sendEmail(@Valid @RequestBody CertRequestDto requestDto) throws Exception {
-
-        String confirm = certService.sendMessage(requestDto);
-
-        return confirm;
+    public void sendEmail(@Valid @RequestBody CertRequestDto requestDto) throws Exception {
+        certService.sendMessage(requestDto);
     }
 
     @PostMapping("/user/check-code")
     @Operation(summary = "인증번호 검증 API =>  이메일로 전송한 인증번호와 사용자 인증번호를 검증하는 API 입니다.",
-            description = "json 형식 데이터 -> (String : code)" +
-                    " => 검증 결과에 따라 True or False 를 Return 해줍니다.")
+            description = "json 형식 데이터 -> (String : email, String : code)" +
+                    " => 검증 결과에 따라 True or error 를 Return 해줍니다.")
     public ResponseEntity checkCode(@Valid @RequestBody CertRequestDto requestDto) throws Exception {
-
         boolean check = certService.checkMessage(requestDto);
-
         return ResponseEntity.ok(check);
     }
 
-    @PostMapping("/user/check-mail")
-    @Operation(summary = "이메일 중복검사 API =>  이메일 중복 검사하는 API 입니다.",
+
+    @DeleteMapping("/user/{id}")
+    @Operation(summary = "유저 탈퇴 API =>  유저 탈퇴하는 API 입니다.",
+            description = "PathVariable 형식 데이터 -> (int : id)" +
+                    " => 검증 결과에 따라 True or error 를 Return 해줍니다.")
+    public ResponseEntity deleteUser(@Valid @PathVariable int id) throws Exception {
+        boolean check = userService.deleteUser(id);
+        return ResponseEntity.ok(check);
+    }
+
+    @PutMapping("/user/{id}/nickname")
+    @Operation(summary = "유저 닉네임 수정 API =>  유저 닉네임 수정 API 입니다.",
+            description = "PathVariable 형식 데이터 + json 형식 데이터 -> (int : id) + (String : nickname)" +
+                    " => 검증 결과에 따라 UserResponseDto or error 를 Return 해줍니다.")
+    public ResponseEntity<UserResponseDto> modifyNickname(@Valid @PathVariable int id, @RequestBody UserRequestDto requestDto) throws Exception {
+        UserResponseDto userResponseDto = null;
+        userResponseDto = userService.modifyNickname(id,requestDto);
+        return ResponseEntity.ok(userResponseDto);
+    }
+
+    @PostMapping("/user/temp-password")
+    @Operation(summary = "임시 비밀번호 발급 API =>  이메일로 임시로 재설정한 비밀번호를 보내주는 API 입니다.",
             description = "json 형식 데이터 -> (String : email)" +
-                    " => 검증 결과에 따라 True or False 를 Return 해줍니다.")
-    public ResponseEntity checkMail(@Valid @RequestBody UserRequestDto requestDto) throws Exception {
-
-        boolean check = userService.checkMail(requestDto);
-
-        return ResponseEntity.ok(check);
+                    " => void Return 해줍니다.")
+    public void tempPassword(@Valid @RequestBody UserRequestDto requestDto) throws Exception {
+        userService.tempPassword(requestDto);
     }
 
-    @PostMapping("/user/check-nick")
-    @Operation(summary = "닉네임 중복검사 API =>  닉네임 중복 검사하는 API 입니다.",
-            description = "json 형식 데이터 -> (String : nickname)" +
-                    " => 검증 결과에 따라 True or False 를 Return 해줍니다.")
-    public ResponseEntity checkNickname(@Valid @RequestBody UserRequestDto requestDto) throws Exception {
-
-        boolean check = userService.checkNickname(requestDto);
-
-        return ResponseEntity.ok(check);
-    }
-
-//    @DeleteMapping("/user/{id}")
-//    @Operation(summary = "닉네임 중복검사 API =>  닉네임 중복 검사하는 API 입니다.",
-//            description = "json 형식 데이터 -> (String : nickname)" +
-//                    " => 검증 결과에 따라 True or False 를 Return 해줍니다.")
-//    public ResponseEntity checkNickname(@Valid @RequestBody UserRequestDto requestDto) throws Exception {
-//
-//        boolean check = userService.checkNickname(requestDto);
-//
-//        return ResponseEntity.ok(check);
-//    }
-
-//    @GetMapping("/user")
-////    @PreAuthorize("hasAnyRole('USER','ADMIN')")
-//    public ResponseEntity<UserDto> getMyUserInfo(HttpServletRequest request) {
-//        return ResponseEntity.ok(userService.getMyUserWithAuthorities());
-//    }
-//
-//    @GetMapping("/user/{username}")
-////    @PreAuthorize("hasAnyRole('ADMIN')")
-//    public ResponseEntity<UserDto> getUserInfo(@PathVariable String username) {
-//        return ResponseEntity.ok(userService.getUserWithAuthorities(username));
-//    }
-
+//    만약 권한 쓰고 싶으면 이렇게 사용
+//    @PreAuthorize("hasAnyRole('USER','ADMIN')")
 
 
 }
