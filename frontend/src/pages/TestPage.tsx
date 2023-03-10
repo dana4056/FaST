b5a8305256020bceef282c6969b102d62ca9e6e6
고태진 <thereisnotruth12@gmail.com>
2023-03-10 13:41:53 +0900

    [FE] feat: 롯데타워, 이순신동상 모델 추가

--- frontend/src/pages/TestPage.tsx
+++ frontend/src/pages/TestPage.tsx
@@ -5,15 +5,19 @@ import SeoulTower from '../assets/blender/SeoulTower';
 import Cheomseongdae from '../assets/blender/Cheomseongdae';
 import Gwangandaegyo from '../assets/blender/Gwangandaegyo';
 import ChinaTown from '../assets/blender/ChinaTown';
+import Yisunshin from '../assets/blender/Yisunshin';
+import LotteTower from '../assets/blender/LotteTower';
 
 function TestPage() {
   return (
-    <div>
+    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
       <Landmark Model={Sungnyemun} cameraPosition={[0, 2, 8]} />
       <Landmark Model={SeoulTower} cameraPosition={[0, 2, 9]} />
       <Landmark Model={Cheomseongdae} cameraPosition={[0, 2, 8]} />
       <Landmark Model={Gwangandaegyo} cameraPosition={[0, 2, 5]} />
       <Landmark Model={ChinaTown} cameraPosition={[0, 2, 18]} />
+      <Landmark Model={Yisunshin} cameraPosition={[0, 2, 5.5]} />
+      <Landmark Model={LotteTower} cameraPosition={[0, 2, 6]} />
     </div>
   );
 }
