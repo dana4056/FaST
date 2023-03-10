9ceaa407943eabf656d243698f81e2650eb16329
고태진 <thereisnotruth12@gmail.com>
2023-03-10 14:29:53 +0900

    [FE] feat: 성산일출봉 모델 추가

--- frontend/src/pages/TestPage.tsx
+++ frontend/src/pages/TestPage.tsx
@@ -8,6 +8,7 @@ import ChinaTown from '../assets/blender/ChinaTown';
 import Yisunshin from '../assets/blender/Yisunshin';
 import LotteTower from '../assets/blender/LotteTower';
 import Building63 from '../assets/blender/Building63';
+import Seongsan from '../assets/blender/Seongsan';
 
 function TestPage() {
   return (
@@ -20,6 +21,7 @@ function TestPage() {
       <Landmark Model={Yisunshin} cameraPosition={[0, 2, 5.5]} />
       <Landmark Model={LotteTower} cameraPosition={[0, 2, 6]} />
       <Landmark Model={Building63} cameraPosition={[0, 2, 7]} />
+      <Landmark Model={Seongsan} cameraPosition={[0, 2, 4]} />
     </div>
   );
 }
