21c02bdf9ffb8bb689aa1276feb98a339c8637bb
dhwnsghdhkd <dhwnsghdhkd@naver.com>
2023-03-14 14:14:54 +0900

    [BE] feat : kakao 로그인

--- backend/src/main/java/a402/FaST/Oauth/CustomUserOAuth2Service.java
+++ backend/src/main/java/a402/FaST/Oauth/CustomUserOAuth2Service.java
@@ -1,5 +1,6 @@
-package a402.FaST.auth;
+package a402.FaST.Oauth;
 
+import a402.FaST.auth.PrincipalDetails;
 import a402.FaST.model.entity.Authority;
 import a402.FaST.model.entity.User;
 import a402.FaST.repository.UserRepository;
@@ -8,27 +9,24 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.Map;
-import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
-public class userOAuth2Service extends DefaultOAuth2UserService {
+public class CustomUserOAuth2Service extends DefaultOAuth2UserService {
 
     @Autowired
     private UserRepository userRepository;
 
-    private static final Logger logger = LoggerFactory.getLogger(userOAuth2Service.class);
+    private static final Logger logger = LoggerFactory.getLogger(CustomUserOAuth2Service.class);
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
@@ -44,51 +42,48 @@ public class userOAuth2Service extends DefaultOAuth2UserService {
     }
 
     private OAuth2User processOAuth2User(OAuth2UserRequest userRequest, OAuth2User oAuth2User) {
+
+        // Attribute를 파싱해서 공통 객체로 묶는다. 관리가 편함.
         OAuth2UserInfo oAuth2UserInfo = null;
 
-        if (userRequest.getClientRegistration().getRegistrationId().equals("kakao")){
+        // kakao naver 구분 하기 위한 provider 변수
+        String provider = userRequest.getClientRegistration().getRegistrationId();
+
+        if (provider.equals("kakao")) {
             logger.info("카카오톡 로그인 요청");
-//            oAuth2UserInfo = new KakaoUserInfo((Map)oAuth2User.getAttributes());
+            oAuth2UserInfo = new KakaoUserInfo((Map)oAuth2User.getAttributes());
         }
-//        else if (userRequest.getClientRegistration().getRegistrationId().equals("naver")){
-//            logger.info("네이버 로그인 요청");
-//            oAuth2UserInfo = new NaverUserInfo((Map)oAuth2User.getAttributes().get("response"));
-//        }
+        else if (provider.equals("naver")){
+            logger.info("네이버 로그인 요청");
+            oAuth2UserInfo = new NaverUserInfo((Map)oAuth2User.getAttributes().get("response"));
+        }
         else {
             logger.info("지원하지 않는 로그인 요청입니다");
         }
 
-
-        Map<String, Object> attributes = oAuth2User.getAttributes();
-
-        Map<String, Object> kakao_account = (Map<String, Object>) attributes.get("kakao_account");
-        String email = (String) kakao_account.get("email");
-            logger.info("email : " + email);
-
-        Map<String, Object> properties = (Map<String, Object>) attributes.get("properties");
-        String nickname = (String) properties.get("nickname");
-            logger.info("nickname : " + nickname);
-
-        Map<String, Object> imgs = (Map<String, Object>) attributes.get("properties");
-        String img = (String) imgs.get("profile_image");
-            logger.info("img : " + img);
-
-            if (userRepository.existsByEmail(email)) {
+        User user;
+        if (userRepository.existsByEmail(oAuth2UserInfo.getEmail())) {
+            user = userRepository.findByEmail(oAuth2UserInfo.getEmail()).get();
+            userRepository.save(user);
             logger.info("가입 한적 있음");
         } else {
             Authority authority = Authority.builder()
                     .authorityName("ROLE_USER")
                     .build();
 
-            User user = User.builder()
-                    .email(email)
-    //                    .password(passwordEncoder.encode(requestDto.getPassword()))
-                    .nickname(nickname)
-                    .img_path(img)
+            user = User.builder()
+                    .email(oAuth2UserInfo.getEmail())
+                    //                    .password(passwordEncoder.encode(requestDto.getPassword()))
+                    .nickname(oAuth2UserInfo.getName())
+                    .img_path(oAuth2UserInfo.getProfile())
                     .authorities(Collections.singleton(authority))
                     .build();
             userRepository.save(user);
         }
-    return null;
+        logger.info("성공");
+        logger.info("user , {}", user);
+        logger.info("oAuth2User , {}", oAuth2User.getAttributes());
+
+        return new PrincipalDetails(user, oAuth2User.getAttributes());
     }
 }
