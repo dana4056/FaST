0cb670a85c5cfc8d4c7c6b35c03c428c8a820f88
고태진 <thereisnotruth12@gmail.com>
2023-03-16 10:07:56 +0900

    [FE] feat: 로딩 페이지 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 
 import IntroPage from './pages/IntroPage';
+import LoadingPage from './pages/LoadingPage';
 import Router from './Router';
 
 import './styles/main.scss';
@@ -12,6 +13,7 @@ function App() {
     <RecoilRoot>
       <Routes>
         <Route path="/" element={<IntroPage />} />
+        <Route path="/loading" element={<LoadingPage />} />
         <Route path="/*" element={<Router />} />
       </Routes>
     </RecoilRoot>
