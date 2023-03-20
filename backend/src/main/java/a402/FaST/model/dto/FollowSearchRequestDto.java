9f32eab55954d032f4d74d3b6a38a42b204d31ec
dhwnsghdhkd <dhwnsghdhkd@naver.com>
2023-03-20 16:37:58 +0900

    [BE] feat : dto add

---
+++ backend/src/main/java/a402/FaST/model/dto/FollowSearchRequestDto.java
@@ -0,0 +1,14 @@
+package a402.FaST.model.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class FollowSearchRequestDto {
+
+   private int id;
+
+}
