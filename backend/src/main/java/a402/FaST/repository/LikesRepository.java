688b4d3a09628cc2a463b2b76742ef8fdb1f7055
Oh JUN Ho <dhwnsghdhkd@naver.com>
2023-03-28 23:58:22 +0900

    [BE] fix : likes 수정

--- backend/src/main/java/a402/FaST/repository/LikesRepository.java
+++ backend/src/main/java/a402/FaST/repository/LikesRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface LikesRepository extends JpaRepository<Likes, Integer> {
 
     int countByArticleId(int articleId);
-//    Likes findByIdAndUserId(int id, int userId);
     boolean existsByIdAndUserId(int id, int userId);
 //    int countByArticleId(int articleId);
 }
