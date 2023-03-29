e277e659078904e21ba0da6a4e7dd8cfb0bce9ef
이소민 <esom735@gmail.com>
2023-03-29 14:37:27 +0900

    [FE] feat: 간편로그인 후 res.data.tags 사이즈가 0 이라면 태그 추가 api 연결

--- frontend/src/pages/LoadingPage.tsx
+++ frontend/src/pages/LoadingPage.tsx
@@ -13,7 +13,23 @@ function LoadingPage() {
   // console.log(location.search.split('=')[1]);
 
   const jwt = location.search.split('=')[1];
-
+  const tag = [
+    '바다',
+    '산',
+    '액티비티',
+    '강',
+    '유적지',
+    '계곡',
+    '호캉스',
+    '캠핑',
+    '힐링',
+    '배낭여행',
+    '박물관',
+    '자전거',
+    '등산',
+    '휴양지',
+    '도심',
+  ];
   const login = async () => {
     const res = await api.fastLogin(jwt);
     if (res.status === 200) {
@@ -21,6 +37,7 @@ function LoadingPage() {
       // recoil-persist로 localstorage에 user 정보 저장
       if (res.data.tags.length === 0) {
         console.log('ddddddddd');
+        const resTag = await api.registerTag(tag, res.data.id);
       }
       setUser(res.data);
       navigate('/home');
