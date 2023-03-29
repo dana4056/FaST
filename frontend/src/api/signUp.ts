2bf357d1f5fed7eddec84507b8c68ee63778dd5e
고태진 <thereisnotruth12@gmail.com>
2023-03-29 09:08:45 +0900

    built test 7

--- frontend/src/api/signUp.ts
+++ frontend/src/api/signUp.ts
@@ -12,6 +12,7 @@ async function sendEmail(email: string) {
     const res = await api.post(`/user/send-email`, {
       email,
     });
+
     console.log(res.status);
     return res.status;
   } catch (error) {
