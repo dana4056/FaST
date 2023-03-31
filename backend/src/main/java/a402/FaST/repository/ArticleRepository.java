074e5346266a753ddd0638e976f11d7448dccb86
dhwnsghdhkd <dhwnsghdhkd@naver.com>
2023-03-31 15:07:42 +0900

    [BE] fix : articlelisttag SQL 문 수정

--- backend/src/main/java/a402/FaST/repository/ArticleRepository.java
+++ backend/src/main/java/a402/FaST/repository/ArticleRepository.java
@@ -11,7 +11,7 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface ArticleRepository extends JpaRepository<Article, Integer> {
-    @Query(value = "select a.id, a.img_path, a.create_time from article a where a.id in " +
+    @Query(value = "select a.id, a.img_path, a.create_time, a.user_id from article a where a.id in " +
             "(select distinct(ah.article_id) from article_has_tag ah where ah.tag_id in" +
             "(select th.tag_id from tag_has_user th where th.user_id = ?1)) order by a.create_time desc", nativeQuery = true)
     List<ArticleList> ArticleListTag(int userId, Pageable pageable);
