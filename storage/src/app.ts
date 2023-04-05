6585c399066c5ce6ead222bbb3ff13141c763389
고태진 <thereisnotruth12@gmail.com>
2023-04-05 10:28:31 +0900

    [BE] feat: profile, article 이미지 경로 구분

--- storage/src/app.ts
+++ storage/src/app.ts
@@ -1,4 +1,5 @@
 import express, { Express, Request, Response } from 'express';
+import path from 'path';
 import multer from 'multer';
 
 import * as cors from 'cors';
@@ -7,21 +8,33 @@ const app: Express = express();
 
 const port = 6060;
 
+const __dirname = path.resolve();
+
 app.use(cors.default());
 
-const storage = multer.diskStorage({
+const profileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/');
+    console.log(req);
+    cb(null, `images/profiles/`);
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+const articleStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    console.log(req);
+    cb(null, `images/articles/`);
   },
   filename: (req, file, cb) => {
     cb(null, file.originalname);
   },
 });
 
-const upload = multer({ storage });
+const profileUpload = multer({ storage: profileStorage });
+const articleUpload = multer({ storage: articleStorage });
 
-app.post('/upload', upload.single('image'), (req: Request, res: Response) => {
-  console.log(req.file);
+app.post('/upload/profile', profileUpload.single('image'), (req: Request, res: Response) => {
   const file = req.file;
   if (!file) {
     res.status(400).json({ message: '이미지를 업로드해주세요.' });
@@ -30,12 +43,17 @@ app.post('/upload', upload.single('image'), (req: Request, res: Response) => {
     res.status(200).json({ imagePath });
   }
 });
-
-app.get('/image/:imageName', (req: Request, res: Response) => {
-  const imageName = req.params.imagename;
-  const imagePath = 'uploads/' + imageName;
-  res.sendFile(imagePath, { root: __dirname });
+app.post('/upload/article', articleUpload.single('image'), (req: Request, res: Response) => {
+  const file = req.file;
+  if (!file) {
+    res.status(400).json({ message: '이미지를 업로드해주세요.' });
+  } else {
+    const imagePath = file.path;
+    res.status(200).json({ imagePath });
+  }
 });
+app.use('/images/articles', express.static('images/articles'));
+app.use('/images/profiles', express.static('images/profiles'));
 
 app.get('/', (req: Request, res: Response) => {
   res.status(200).send('hello');
